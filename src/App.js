aea26e16c6c1a2b29f07ebb7d6d4bd3b5eb81a71
Bishal Thapa <bishal.thap3@gmail.com>
2023-07-19 13:32:38 +0545

    Dark/Light Mode is implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React,{ useState } from 'react';
 import './App.css';
-
+import About from './Components/About';
+import LearnMore from './Components/LearnMore';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type) => {
+    setAlert({
+      msg: message,
+      typ: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    },1500);
+  }
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark Mode has been Enabled !","success");
+      
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been Enabled !","success");
+    }
+  }
 
   return (
     <>    
-      
+     
     </>
    
   );
