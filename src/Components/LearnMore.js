a8bd7b509f3737d3f3eb7cdb9b6cd7260eeab498
Bishal Thapa <bishal.thap3@gmail.com>
2023-07-19 13:33:15 +0545

    Other Pages are added

---
+++ src/Components/LearnMore.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LearnMore(props) {
+  return (
+    <div>
+        <h1 style={{color: props.mode==='light' ? '#3d3d3d' : 'white'}}>Learn More</h1>
+    </div>
+  )
+}
